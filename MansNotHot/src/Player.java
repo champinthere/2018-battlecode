88e55e1b5ad5ee53301f6d0ea9791029e70478b7
Rahul Yesantharao <rahul.yesantharao@gmail.com>
2018-01-18 23:17:06 -0500

    Deleted everything

--- MansNotHot/src/Player.java
+++ MansNotHot/src/Player.java
@@ -3,12 +3,8 @@ import bc.GameController;
 public class Player {
     public static void main(String[] args) {
         GameController gc = new GameController();
-        GameManager manager = new GameManager(gc);
         while (true) {
-            manager.gameStep();
+            
         }
-//        MapLocation loc1 = new MapLocation(Planet.Earth, 0, 0);
-//        MapLocation loc2 = new MapLocation(Planet.Earth, 0, 4);
-//        StaticPath path = manager.getMapAnalyzer().getStaticPath(loc1, loc2);
     }
 }
